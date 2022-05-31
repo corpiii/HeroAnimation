4ddec2f3684d5f3bd406ee4c5020054f8e8ef58c
jeongmin <lj845854@gmail.com>
2022-05-31 21:50:52 +0900

    add : 레시피 설명 페이지 정의

---
+++ HeroProject/ViewController/DetailPageViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailPageViewController.swift
+//  HeroProject
+//
+//  Created by 이정민 on 2022/05/31.
+//
+
+import UIKit
+import Hero
+import Stevia
+
+class DetailPageViewController: UIViewController {
+
+    let coffeeImage = UIImageView()
+    let coffeeName = UILabel()
+    let coffeePrice = UILabel()
+    let coffeeRecipe = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
